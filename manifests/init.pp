60e51e7084b40505c9992b2222e9f51de7ee9baa
Kris Buytaert <sdog@inuits.be>
2012-11-19 20:41:48 +0100

    Updated README

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,14 @@
 # == Class: kibana-ruby
 #
-1;2P
-# Full description of class kibana-ruby here.
+# This package installs (from rpm)  and configures
+# The Ruby variant of Kibana
+# Assumptions
+# - Kibana- Ruby is available in your repos and installs in
+#   /var/vhosts/ribana/htdocs
+#  - ribana because it's the rubyversion of kibana and we have the php version around too
 #
+#
+
 # === Parameters
 #
 # Document parameters here.
@@ -27,7 +33,12 @@
 #
 # Copyright 2013 Kris Buytaert, unless otherwise noted.
 #
+#
 class kibana-ruby {
 
+
+  package { 'kibana-ruby':
+    ensure => '2-1',
+  }
 
 }
