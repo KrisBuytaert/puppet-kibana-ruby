29b8b8c17296ed16dc1231c838030a939d10b7a6
Kris Buytaert <sdog@inuits.be>
2012-11-19 20:24:34 +0100

    Initial commti

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+# == Class: kibana-ruby
+#
+1;2P
+# Full description of class kibana-ruby here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+#
+# === Examples
+#
+#  class { ruby:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  }
+#
+# === Authors
+#
+# Author Name <github@krisbuytaert.be
+#
+# === Copyright
+#
+# Copyright 2013 Kris Buytaert, unless otherwise noted.
+#
+class kibana-ruby {
+
+
+}
